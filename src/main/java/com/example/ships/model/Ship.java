ee617c5d2f48a6ea357bbebb593270a461869cb6
Sylwia Krzysztoń <sylwia.krzy64@gmail.com>
2023-12-07 16:21:15 +0100

    add: export all ships from database to json file

--- src/main/java/com/example/ships/model/Ship.java
+++ src/main/java/com/example/ships/model/Ship.java
@@ -3,6 +3,7 @@ package com.example.ships.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
@@ -21,70 +22,72 @@ import java.time.LocalTime;
 @JsonIgnoreProperties
 public class Ship {
     @Id
+    @Expose(serialize = true, deserialize = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
-    @JsonProperty("Kod portu")
+    @Expose(serialize = true, deserialize = false)
     @SerializedName("Kod portu")
     private String finalPortCode = "PLSWI";
 
+    @Expose(serialize = true, deserialize = false)
     @NotNull(message = "Data przybycia statku nie może być pusta")
     @DateTimeFormat(pattern = "dd.MM.yyyy")
-    @JsonProperty("Data przybycia")
     @SerializedName("Data przybycia")
     public LocalDate arrivalDate;
 
+    @Expose(serialize = true, deserialize = false)
     @NotNull(message = "Godzina nie może być pusta")
     @JsonFormat(pattern = "HH:mm:ss")
-    @JsonProperty("Godzina przybycia")
     @SerializedName("Godzina przybycia")
     public LocalTime arrivalTime;
 
+    @Expose(serialize = true, deserialize = false)
     @Size(min = 5, max = 5, message = "Kod portu musi zawierać dokładnie 5 znaków")
     @Pattern(regexp = "^[A-Z]*$", message = "Kod portu zawiera nieprawidłowe znaki")
     @NotNull(message = "Kod portu nie może być pusty")
-    @JsonProperty("Kod portu, z którego jednostka przypłynęła")
     @SerializedName("Kod portu, z którego jednostka przypłynęła")
     public String initialPortCode;
 
+    @Expose(serialize = true, deserialize = false)
     @Size(max = 50, message = "Nazwa statku może zawierać maksymalnie 50 znaków")
     @NotNull(message = "Nazwa statku nie może być pusta")
     @Pattern(regexp = "^[a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ .-]*$", message = "Nazwa statku zawiera nieprawidłowe znaki")
-    @JsonProperty("Nazwa statku")
     @SerializedName("Nazwa statku")
     public String shipName;
 
+    @Expose(serialize = true, deserialize = false)
     @Size(max = 50, message = "Bandera może zawierać maksymalnie 50 znaków")
     @NotNull(message = "Bandera nie może być pusta")
     @Pattern(regexp = "^[a-zA-Z ]*$", message = "Bandera zawiera nieprawidłowe znaki")
-    @JsonProperty("Bandera")
     @SerializedName("Bandera")
     public String flag;
 
+    @Expose(serialize = true, deserialize = false)
     @NotNull(message = "Pojemność brutto nie może być pusta")
-    @JsonProperty("Pojemność brutto")
     @SerializedName("Pojemność brutto")
     public double grossCapacity;
 
+    @Expose(serialize = true, deserialize = false)
     @NotNull(message = "Dlugosc nie może być pusta")
-    @JsonProperty("Długość")
     @SerializedName("Długość")
     public double length;
 
+    @Expose(serialize = true, deserialize = false)
     @NotNull(message = "Nazwa agenta nie może byc pusta")
     @Size(max = 100, message = "Nazwa agenta nie może przekraczać 100 znaków")
     @Pattern(regexp = "^[a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ .-]*$", message = "Agent zawiera nieprawidłowe znaki")
-    @JsonProperty("Agent")
     @SerializedName("Agent")
     public String agent;
 
+    @Expose(serialize = true, deserialize = false)
     @Size(max = 50, message = "Dlugosc nazwy ndabrzeza nie moze przekraczac 50 znakow")
     @NotNull(message = "Nazwa nadbrzeza nie moze byc pusta")
     @Pattern(regexp = "^[a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ .-]*$", message = "Nadbrzeze zawiera nieprawidłowe znaki")
-    @JsonProperty("Nabrzeże (miejsce zacumowania)")
     @SerializedName("Nabrzeże (miejsce zacumowania)")
     public String mooringPlace;
 
+    @Expose(serialize = false, deserialize = false)
     @ManyToOne
     private User user;
 
--- src/main/java/com/example/ships/service/FileService.java
+++ src/main/java/com/example/ships/service/FileService.java
@@ -6,18 +6,27 @@ import com.example.ships.model.Ship;
 import com.example.ships.repo.ShipRepository;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.time.LocalDate.*;
 
 
 @Service
@@ -46,8 +55,64 @@ public class FileService
         return filteredShips;
     }
 
-    public String writeToJsonFile(List <Ship> ships)
-    {
-        return null;
-    };
+    public List<Ship> parseDate(List<Ship> ships) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        for (Ship ship : ships) {
+            String timeString = String.valueOf(ship.getArrivalTime());
+            LocalTime arrivalTime = LocalTime.parse(timeString, timeFormatter);
+
+            String dateString = String.valueOf(ship.getArrivalDate());
+            LocalDate arrivalDate = parse(dateString, inputFormatter);
+            String formattedDate = arrivalDate.format(outputFormatter);
+
+            ship.setArrivalDate(parse(formattedDate, outputFormatter));
+            ship.setArrivalTime(arrivalTime);
+        }
+
+        return ships;
+    }
+
+    public boolean writeToJsonFile(List<Ship> ships) throws IOException {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .registerTypeAdapter(LocalTime.class, new LocalTimeAdapter())
+                .excludeFieldsWithoutExposeAnnotation()
+                .serializeNulls()
+                .create();
+
+        List<Ship> newShips = parseDate(ships);
+        System.out.println("Number of ships: " + newShips.size());
+
+        LocalDate currentDate = LocalDate.now();
+        LocalTime currentTime = LocalTime.now();
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH-mm-ss");
+
+        String formattedDate = currentDate.format(dateFormatter);
+        String formattedTime = currentTime.format(timeFormatter);
+
+        String fileName = "exportedShips_"+formattedDate + "_" + formattedTime + ".json";
+        Path filePath = Paths.get("src/main/resources/exportFiles", fileName);
+
+        if(newShips.isEmpty())
+        {
+            return false;
+        }
+
+        try (FileWriter writer = new FileWriter(filePath.toAbsolutePath().toString()))
+        {
+            gson.toJson(newShips, writer);
+            writer.flush();
+        } catch (JsonIOException | IOException e)
+        {
+            return false;
+        }
+        return true;
+    }
+
+
 }
